5b9401f8c811c5c2c816e84f248fbd44396b9e1d
kevingrad <70232994+kevingrad@users.noreply.github.com>
2021-12-14 08:25:23 -0800

    Update pwm.h

--- pwm.h
+++ pwm.h
@@ -3,7 +3,10 @@
 #include <Arduino.h>
 #include <avr/io.h>
 
+// Initializes our PWM using timer 3 for the SG90 servo movement.
 void initPWM();
+
+// Only two valid parameters, moves the SG90 servo to either the locked or unlocked position.
 void Turn(int percent);
 
 #endif
