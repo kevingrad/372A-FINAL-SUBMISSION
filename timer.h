4e365994d4286edb1ff2b0198741d06f19b70be6
kevingrad <70232994+kevingrad@users.noreply.github.com>
2021-12-14 08:23:02 -0800

    Create timer.h

---
+++ timer.h
@@ -0,0 +1,13 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <avr/io.h>
+
+void initTimer1();
+void delayUs(unsigned int delay);
+void initTimer0();
+// void delayS(unsigned int delay); 
+// void initTimer4();
+void delayMs(unsigned int delay);
+
+#endif
