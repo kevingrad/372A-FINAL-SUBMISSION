20544bd4bb69f05ccc518bc8818541529ce367c2
kevingrad <70232994+kevingrad@users.noreply.github.com>
2021-12-14 08:22:23 -0800

    Create pwm.cpp

---
+++ pwm.cpp
@@ -0,0 +1,33 @@
+#include "pwm.h"
+
+void initPWM() {
+
+    DDRE |= (1 << DDE5);
+
+    // MODE 15 ON PWM TIMER4
+    TCCR3A |= (1 << WGM30) | (1 << WGM31);
+    TCCR3B |= (1 << WGM33) | (1 << WGM32);
+    
+    // NON-INVERTING MODE WITH TCCR3A = 10
+    TCCR3A &= ~(1 << COM3C0);
+    TCCR3A |= (1 << COM3C1);
+    OCR3A = 5000;
+    OCR3C = 0;
+    
+    // PRESCALER 64
+    TCCR3B |= (1 << CS30);
+    TCCR3B |= (1 << CS31);
+    TCCR3B &= ~(1 << CS32);
+}
+
+void Turn(int way){
+    if (way == 500) {
+        OCR3C = OCR3A * 0.05;
+        // Serial.println("Unlocking");
+    }
+    else if (way == 1000) {
+        OCR3C = OCR3A * 0.1; 
+        // Serial.println("Locking");      
+    }
+        
+}
