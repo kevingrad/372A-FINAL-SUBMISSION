c58c37f23beffc658756a0e9af44c596575fb032
kevingrad <70232994+kevingrad@users.noreply.github.com>
2021-12-14 08:21:05 -0800

    Create lcd.h

---
+++ lcd.h
@@ -0,0 +1,13 @@
+#ifndef LCD_H
+#define LCD_H
+
+void initLCD();
+void fourBitCommandWithDelay(unsigned char data, unsigned int delay);
+void eightBitCommandWithDelay(unsigned char command, unsigned int delay);
+void writeCharacter(unsigned char character);
+void writeString(const char *string);
+void moveCursor(unsigned char x, unsigned char y);
+void initLCDPins();
+void initLCDProcedure();
+
+#endif
